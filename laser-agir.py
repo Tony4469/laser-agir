d9a8647639e0426616e06b42e49f72d6d5a3b9f1
Soufiane <souflam007@yahoo.fr>
2019-06-16 01:34:44 +0200

    first commit

---
+++ laser-agir.py
@@ -0,0 +1,93 @@
+from os import  getcwd, path
+import os
+from os import listdir
+from os.path import isfile, join
+
+import time
+import json
+
+from flask import Flask, request
+from flask_restful import Resource, Api
+
+from threading import Timer
+import urllib 
+
+from source.similarity_search import Similarity
+
+app = Flask(__name__)
+api = Api(app)
+
+import subprocess
+
+#On définit notre environnement de travail :
+local=False
+if os.environ["TERM_PROGRAM"] == "Apple_Terminal":
+    local=True
+    
+class LASER(Resource):
+    def __init__(self):
+        super().__init__()
+        print('initializing')
+    
+    def start(self):
+        #Désactivé pour l'instant
+        #similarity = Similarity()
+        return False
+    
+    def post(self):
+        print(request.json)
+        print(request.json['sentences'])
+        t0 = time.process_time()
+        
+        #On enregistre les phrases dans le fichier langue associé
+#        sentences = request.json['sentences']
+#        sentences = [ tuple(sentences[x]) for x in range(len(sentences))]
+        
+        similarity = Similarity(lang=['en', 'fr'], p_path=os.getcwd())
+        distances, indexes, cosine = similarity.launch()
+
+        print(time.process_time() - t0, "seconds process time")
+        return json.dumps({'distances': distances, 'indexes': indexes, 'cosine': cosine.tolist(), 'time': time.process_time() - t0})
+    
+    def get(self):
+        return {'employees': "got"}
+
+
+print('initialized')
+#q = Queue(connection=conn)
+#
+#result = q.enqueue(count_words_at_url, 'http://heroku.com')
+#print('resultt', result.get_id())
+
+
+def begin():
+    print("beginning model")
+    algo=LASER()
+    print("ok dwnld")
+    
+t = Timer(1.0, begin)
+t.start() # after 15 seconds, starting process
+
+print('launching')
+
+api.add_resource(LASER, '/laser') # Route_1
+
+if __name__ == '__main__':
+    print('dnas main')
+    app.run(port="5001")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ source/similarity_search.py
@@ -0,0 +1,95 @@
+#!/usr/bin/python3
+# Copyright (c) Facebook, Inc. and its affiliates.
+# All rights reserved.
+#
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree.
+#
+# LASER  Language-Agnostic SEntence Representations
+# is a toolkit to calculate multilingual sentence embeddings
+# and to use them for document classification, bitext filtering
+# and mining
+#
+# --------------------------------------------------------
+#
+# Quora Q&A paraphrase detection
+
+import os
+import sys
+import numpy as np
+
+# get environment
+LASER = os.getenv('LASER_AGIR', "/Users/tonyparker/Documents/Python/laser-agir/")
+
+sys.path.append(LASER + '/source')
+sys.path.append(LASER + '/source/lib')
+from embed import SentenceEncoder, EncodeLoad, EncodeFile
+from text_processing import Token, BPEfastApply
+from indexing import IndexCreate, IndexSearchMultiple, IndexPrintConfusionMatrix
+
+###############################################################################
+
+print('LASER: similarity search')
+
+print('\nProcessing:')
+all_texts = []
+
+class Namespace:
+    def __init__(self, **kwargs):
+        self.__dict__.update(kwargs)
+        
+class Similarity:
+    def __init__(self, lang, p_path):
+        self.args = Namespace(
+                base_dir=os.path.normpath(os.path.join(p_path, './tasks/similarity') ), 
+                bpe_codes=os.path.normpath(os.path.join(p_path, './models/93langs.fcodes') ), 
+                buffer_size=100, 
+                cpu=False, 
+                data= os.path.normpath(os.path.join(p_path, './tasks/similarity/dev/souf') ), 
+                encoder= os.path.normpath(os.path.join(p_path, './models/bilstm.93langs.2018-12-26.pt') ), 
+                lang= lang,  #Voir pour passer ce paramètre seulement au moment du lancement
+                max_sentences=None, 
+                max_tokens=12000, 
+                output= os.path.normpath(os.path.join(p_path, './tasks/similarity/embed/souf') ), 
+                textual=False, 
+                verbose=True)
+        self.enc = EncodeLoad(self.args)
+        out_dir = os.path.dirname(self.args.output)
+        if not os.path.exists(out_dir):
+            print(' - creating directory {}'.format(out_dir))
+            os.mkdir(out_dir)
+    
+    def launch(self):
+        all_data = []
+        all_index = []
+        for l in self.args.lang:
+            Token(os.path.join(self.args.base_dir, self.args.data + '.' + l),
+                  os.path.join(self.args.base_dir, self.args.output + '.tok.' + l),
+                  lang=l,
+                  romanize=True if l == 'el' else False,
+                  lower_case=True,
+                  verbose=self.args.verbose, over_write=False)
+            BPEfastApply(os.path.join(self.args.base_dir, self.args.output + '.tok.' + l),
+                         os.path.join(self.args.base_dir, self.args.output + '.bpe.' + l),
+                         self.args.bpe_codes,
+                         verbose=self.args.verbose, over_write=False)
+            EncodeFile(self.enc,
+                       os.path.join(self.args.base_dir, self.args.output + '.bpe.' + l),
+                       os.path.join(self.args.base_dir, self.args.output + '.enc.' + l),
+                       verbose=self.args.verbose, over_write=False)
+            d, idx = IndexCreate(os.path.join(self.args.base_dir, self.args.output + '.enc.' + l),
+                                 'FlatL2',
+                                 verbose=self.args.verbose, save_index=False)
+            all_data.append(d)
+            all_index.append(idx)
+        
+        
+        distances, indexes, cosine = IndexSearchMultiple(all_data, all_index, texts=all_texts,
+                                  verbose=True, print_errors=False)
+        
+        print('D', distances)
+        print('I', indexes)
+        print('cosine', cosine)
+        
+        return distances, indexes, cosine
+
